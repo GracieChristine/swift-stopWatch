028e42f7b0e7b557430123d1c7cc75c4f18e9de0
GracieChristine <graciechristineli@gmail.com>
2018-01-06 11:45:42 -0700

    Start project.

---
+++ stopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stopWatch
+//
+//  Created by Gracie Christine on 1/6/18.
+//  Copyright © 2018 Gracie Christine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
