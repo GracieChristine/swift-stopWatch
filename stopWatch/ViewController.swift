18ec88bc72ea7263b47609d94dbcff46f95c67df
GracieChristine <graciechristineli@gmail.com>
2018-01-06 12:11:38 -0700

    Working on start, pause, reset functionalities. Not working.

--- stopWatch/ViewController.swift
+++ stopWatch/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var label: UILabel!
+    var time = 0;
+    
+    // Timer
+    var timer = Timer();
+    
+    @IBAction func start(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true);
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate();
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        timer.invalidate();
+        time = 0;
+    }
+    
+    @objc func action() {
+        time += 1;
+        label.text = String(time);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
